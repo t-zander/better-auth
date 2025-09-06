1ecb27e07d3afbc6b36472ab41665b8dd193a239
t-zander <alex.sergeevich.tarasenko@gmail.com>
2025-09-06 23:15:39 +0200

    add a super basic email password auth

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -1,12 +1,16 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../db/db";
+import * as schema from "@/lib/db/schemas/auth-schema";
 
 export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
+    requireEmailVerification: false,
+    minPasswordLength: 6,
   },
   database: drizzleAdapter(db, {
     provider: "sqlite", // or "mysql", "sqlite"
+    schema,
   }),
 });
--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
-import { drizzle } from "drizzle-orm/bun-sqlite";
-import { Database } from "bun:sqlite";
+import Database from "better-sqlite3";
+import { drizzle } from "drizzle-orm/better-sqlite3";
 
 const sqlite = new Database(process.env.DB_FILE_NAME!);
 
-export const db = drizzle({ client: sqlite });
+export const db = drizzle(sqlite);
