3ecb988946944d68b318db26da662a82a948846a
t-zander <alex.sergeevich.tarasenko@gmail.com>
2025-10-11 23:34:05 +0200

    handle sign on with email redirect

--- src/app/(auth)/utils.ts
+++ src/app/(auth)/utils.ts
@@ -1,3 +1,9 @@
-export function getSignUpRedirectUrl() {
+export function getSignUpRedirectUrl(userRole: string) {
+  const roles = userRole.split(",");
+
+  if (roles.includes("shelterOwner") || roles.includes("admin")) {
+    return "/dashboard";
+  }
+
   return "/role-selector";
 }
